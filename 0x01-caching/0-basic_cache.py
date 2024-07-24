5cd5dc5ec929978bd17707095294eb48e8246380
daveyhmariam <daveyhmariam@gmail.com>
2024-07-25 01:29:16 +0300

    bug fix

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -13,12 +13,13 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """ Add an item in the cache
         """
-        self.cache_data[key] = item
+        if key and item:
+            self.cache_data[key] = item
 
     def get(self, key):
         """ Get an item by key
         """
-        if key in self.cache_data.keys():
+        if key and key in self.cache_data.keys():
             return self.cache_data[key]
         else:
             return None
