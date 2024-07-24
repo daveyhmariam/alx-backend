763447745a8c881755b3c3635305195fa666e863
daveyhmariam <daveyhmariam@gmail.com>
2024-07-25 02:52:25 +0300

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Caching system with FIFO algorithm
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """
+    FIFO Caching class Inherits from BaseCaching class
+    Implements get and put methon of parent class
+    """
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Add an item in the cache
+        """
+        if key and item:
+            if key in self.queue:
+                self.queue.remove(key)
+            elif len(self.cache_data) >= self.MAX_ITEMS:
+                print(f"DISCARD: {self.queue[0]}")
+                del self.cache_data[self.queue[0]]
+                self.queue.pop(0)
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key and key in self.cache_data.keys():
+            return self.cache_data[key]
+        else:
+            return None
