0d7c342e4c3c69a926379d9360817ab3e85f7c7c
daveyhmariam <daveyhmariam@gmail.com>
2024-07-27 03:57:00 +0300

    returns given page

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import csv
 import math
 from typing import List
@@ -26,8 +25,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """
+        Return page data
+        """
         assert type(page) is int and page > 0
-        assert type(page_size) is int and page > 0
+        assert type(page_size) is int and page_size > 0
 
         dataset = self.dataset()
         indeces = index_range(page, page_size)
