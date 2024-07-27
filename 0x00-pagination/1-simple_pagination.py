8ba98b36fc02201652363fd7d458a4a13af171f4
daveyhmariam <daveyhmariam@gmail.com>
2024-07-27 03:49:06 +0300

    returns given page

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+
+import csv
+import math
+from typing import List
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page > 0
+
+        dataset = self.dataset()
+        indeces = index_range(page, page_size)
+        try:
+            return dataset[indeces[0]: indeces[1]]
+        except IndexError:
+            return []
+
+
+def index_range(page, page_size):
+    """
+    Args:
+        page (int): page num
+        page_size (int): page size
+    Return:
+        tuple: size two, range of indece
+    """
+    index = (page - 1) * page_size
+    return (index, index + page_size)
