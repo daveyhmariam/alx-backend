de8bc818418bab45b7d14056c6cdacb3670e22a1
daveyhmariam <daveyhmariam@gmail.com>
2024-08-29 00:52:41 +0300

    hashing in redis

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,26 @@
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+client.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', function(err) {
+  console.log(`Redis client not connected to the server: ${err}`);
+});
+
+client.hset('HolbertonSchools', 'Portland', '50', print);
+client.hset('HolbertonSchools', 'Seattle', '80', print);
+client.hset('HolbertonSchools', 'New York', '20', print);
+client.hset('HolbertonSchools', 'Bogota', '20', print);
+client.hset('HolbertonSchools', 'Cali', '40', print);
+client.hset('HolbertonSchools', 'Paris', '2', print);
+
+client.hgetall('HolbertonSchools', function(error, result) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(result);
+});
